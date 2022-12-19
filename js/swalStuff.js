0c3242f4074940fc4688296f71ce2a79000afb28
TheGreaterArki <121049633+TheGreaterArki@users.noreply.github.com>
2022-12-19 22:25:40 +0530

    Added files

---
+++ js/swalStuff.js
@@ -0,0 +1,9 @@
+function noID(){
+    swal("No Identification!","Please Request The Password From The Host To Join The Program")
+}
+function wrongPassword(){
+    swal("Incorrect Password!","The Password You Have Entered Is Incorrect. Please Recheck for Spelling Errors and/or Incorrect Capitalisation")
+}
+function correctPassword(){
+    swal("Success!", "Please Wait For The Redirect")
+}
